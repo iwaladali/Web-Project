434131f8cdf4429f5b7616f663e9e21061007157
Ibrahim Waladali <ibrahimwaldali@hotmail.com>
2026-06-01 18:15:08 +0300

    Add Tabels

--- SubscriptionHub/SubscriptionHub/Models/Service.cs
+++ SubscriptionHub/SubscriptionHub/Models/Service.cs
@@ -14,7 +14,7 @@ namespace SubscriptionHub.Models
         
         public string? Description { get; set; }
 
-        
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
         
