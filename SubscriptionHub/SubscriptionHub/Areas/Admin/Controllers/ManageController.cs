9b102ab4c52da0749689be49459fdbe01c2ba196
dimasa8 <samerdema16@gmail.com>
2026-06-16 14:10:10 +0300

    Admin

---
+++ SubscriptionHub/SubscriptionHub/Areas/Admin/Controllers/ManageController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SubscriptionHub.Data;
+using SubscriptionHub.Models;
+
+namespace SubscriptionHub.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ManageController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ManageController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ManageUsers()
+        {
+            var users = _context.Users.ToList();
+            return View(users);
+        }
+
+        public IActionResult ManageServices()
+        {
+            var services = _context.Services.ToList();
+            return View(services);
+        }
+
+        public IActionResult ManageCategories()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult ManageSubscriptions()
+        {
+            ViewBag.Services = _context.Services.ToList();
+
+            var subscriptions = _context.Subscriptions
+                .Include(s => s.User)
+                .Include(s => s.Service)
+                .ToList();
+
+            return View(subscriptions);
+        }
+    }
+}
